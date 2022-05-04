f1698ca906df6da589929e6003ad36b27655ac4a
Tim <97968652+40479269@users.noreply.github.com>
2022-05-04 22:31:35 +0100

    Update scores.js

--- js/scores.js
+++ js/scores.js
@@ -27,7 +27,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
-
-    console.log(highScores);
+    window.location.assign('/brainbox/submitscore.html');
 };
